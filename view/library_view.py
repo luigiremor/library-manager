3f6bfe2516fe3b17060934d8e6464b464e5b540b
Erik Orsolin <depaulaerik670@gmail.com>
2023-06-24 20:17:44 -0300

    Feat: create LibraryView

---
+++ view/library_view.py
@@ -0,0 +1,15 @@
+import tkinter
+import customtkinter
+
+class LibraryView:
+    def menu_auth():
+        pass
+
+    def menu_collection():
+        pass
+
+    def menu_lend():
+        pass
+
+    def menu_students():
+        pass

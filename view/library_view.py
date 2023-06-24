7e5bede4e721d99dd37c937058b49998b1a04fdf
Erik Orsolin <depaulaerik670@gmail.com>
2023-06-24 20:26:47 -0300

    Feat: setting the window attribute

--- view/library_view.py
+++ view/library_view.py
@@ -2,6 +2,12 @@ import tkinter
 import customtkinter
 
 class LibraryView:
+    def __init__(self):
+        customtkinter.set_appearance_mode("dark")
+        customtkinter.set_default_color_theme("dark-blue")
+        self.window = customtkinter.CTk()
+        self.window.title("Library Management System")
+
     def menu_auth():
         pass
 

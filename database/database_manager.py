92d5999b2da4aada21548f4b14591f60e89a480c
Luigi Remor <luigiremor@gmail.com>
2023-06-25 18:01:19 -0300

    feat: add update methods for items

--- database/database_manager.py
+++ database/database_manager.py
@@ -8,6 +8,8 @@ class DatabaseManager(BaseTableManager):
         super().__init__(db_name)
         self.create_tables()
 
+    # Tables
+
     def create_tables(self):
         # Create tables for Library, Person, Item, Reservation, Lend etc.
         self.create_table_librarian()
@@ -50,7 +52,7 @@ class DatabaseManager(BaseTableManager):
             CREATE TABLE IF NOT EXISTS items (
                 id INTEGER PRIMARY KEY,
                 title TEXT NOT NULL,
-                release_year TEXT NOT NULL,
+                release_year INTEGER NOT NULL,
                 is_lend INTEGER NOT NULL DEFAULT 0,
                 is_reserved INTEGER NOT NULL DEFAULT 0,
                 id_student_lent INTEGER,
@@ -130,6 +132,8 @@ class DatabaseManager(BaseTableManager):
         """)
         self.conn.commit()
 
+    # CRUD
+
     def get_librarian_password(self, email):
         password = self.select_one('librarians', 'email', email)['password']
         return password
@@ -150,30 +154,14 @@ class DatabaseManager(BaseTableManager):
         self.insert('students', ['name', 'email', 'cpf', 'tel', 'registration'], [
                     name, email, cpf, tel, registration])
 
-    def get_librarian(self, email):
+    def get_librarian_by_email(self, email):
         return self.select_one('librarians', 'email', email)
 
-    def get_student(self, registration):
+    def get_student_by_registration(self, registration):
         return self.select_one('students', 'registration', registration)
     
     def get_all_students(self):
         return self.select_all('students')
-    
-    def get_book_item(self, item_id):
-        self.cursor.execute("""
-            SELECT items.*, book_items.author 
-            FROM items
-            JOIN book_items ON items.id = book_items.id_item
-            WHERE items.id = ?
-        """, (item_id,))
-        result = self.cursor.fetchone()
-
-        if result:
-            columns = [column[0] for column in self.cursor.description]
-            return dict(zip(columns, result))
-        
-        return None
-
     
     def create_book_item(self, title, author, release_year):
         self.insert('items', ['title', 'release_year'], [title, release_year])
@@ -196,7 +184,22 @@ class DatabaseManager(BaseTableManager):
 
         return last_inserted_id
 
-    def get_magazine_item(self, item_id):
+    def get_book_item_by_id(self, item_id):
+        self.cursor.execute("""
+            SELECT items.*, book_items.author 
+            FROM items
+            JOIN book_items ON items.id = book_items.id_item
+            WHERE items.id = ?
+        """, (item_id,))
+        result = self.cursor.fetchone()
+
+        if result:
+            columns = [column[0] for column in self.cursor.description]
+            return dict(zip(columns, result))
+        
+        return None
+
+    def get_magazine_item_by_id(self, item_id):
         self.cursor.execute("""
             SELECT items.*, magazine_items.*
             FROM items
@@ -211,7 +214,7 @@ class DatabaseManager(BaseTableManager):
         
         return None
 
-    def get_article_item(self, item_id):
+    def get_article_item_by_id(self, item_id):
         self.cursor.execute("""
             SELECT items.*, article_items.*
             FROM items
@@ -225,6 +228,52 @@ class DatabaseManager(BaseTableManager):
             return dict(zip(columns, result))
         
         return None
+
+    def update_book_item(self, item_id, title, author, release_year):
+        self.cursor.execute("""
+            UPDATE items
+            SET title = ?, release_year = ?
+            WHERE id = ?
+        """, (title, release_year, item_id))
+
+        self.cursor.execute("""
+            UPDATE book_items
+            SET author = ?
+            WHERE id_item = ?
+        """, (author, item_id))
+
+        self.conn.commit()
+
+    def update_magazine_item(self, item_id, release_year, title, publisher, pages_count, language, genre):
+        self.cursor.execute("""
+            UPDATE items
+            SET title = ?, release_year = ?
+            WHERE id = ?
+        """, (title, release_year, item_id))
+
+        self.cursor.execute("""
+            UPDATE magazine_items
+            SET publisher = ?, pages_count = ?, language = ?, genre = ?
+            WHERE id_item = ?
+        """, (publisher, pages_count, language, genre, item_id))
+
+        self.conn.commit()
+
+    def update_article_item(self, item_id, release_year, title, abstract, word_count, author, language, keywords):
+        self.cursor.execute("""
+            UPDATE items
+            SET title = ?, release_year = ?
+            WHERE id = ?
+        """, (title, release_year, item_id))
+
+        self.cursor.execute("""
+            UPDATE article_items
+            SET abstract = ?, word_count = ?, author = ?, language = ?, keywords = ?
+            WHERE id_item = ?
+        """, (abstract, word_count, author, language, keywords, item_id))
+
+        self.conn.commit()
+
 
     def reserve_item(self, item_id, student_id):
         self.cursor.execute("""
@@ -258,6 +307,9 @@ class DatabaseManager(BaseTableManager):
     def get_all_lends(self):
         return self.select_all('lends')
 
+    def get_all_reservations(self):
+        return self.select_all('reservations')
+
     def cancel_item_reservation(self, item_id):
         self.cursor.execute("""
             UPDATE items
@@ -273,7 +325,6 @@ class DatabaseManager(BaseTableManager):
             WHERE id = ?
         """, (item_id,))
         self.conn.commit()
-
 
     def get_student_who_borrowed_item(self, item_id):
         self.cursor.execute("""
@@ -291,7 +342,24 @@ class DatabaseManager(BaseTableManager):
 
         return None
 
+    def get_all_students_who_borrowed_item(self):
+        self.cursor.execute("""
+            SELECT students.* 
+            FROM items
+            JOIN students ON items.id_student_lent = students.registration
+            GROUP BY students.registration
+        """)
+        result = self.cursor.fetchall()
 
+        if result:
+            columns = [column[0] for column in self.cursor.description]
+            students = []
+            for row in result:
+                students.append(dict(zip(columns, row)))
+            return students
+
+        return None
+        
     def get_student_who_reserved_item(self, item_id):
         self.cursor.execute("""
             SELECT students.* 
@@ -307,6 +375,24 @@ class DatabaseManager(BaseTableManager):
         
         return None
 
+    def get_all_students_who_reserved_item(self):
+        self.cursor.execute("""
+            SELECT students.*
+            FROM items
+            JOIN students ON items.id_student_reserved = students.registration
+            GROUP BY students.registration
+        """)
+        result = self.cursor.fetchall()
+
+        if result:
+            columns = [column[0] for column in self.cursor.description]
+            students = []
+            for row in result:
+                students.append(dict(zip(columns, row)))
+            return students
+        
+        return None
+    
     # Implement similar methods for all operations (insert, update, delete, select) on each table
     # For example, insert_item, delete_item, select_item, update_item etc.
 
--- tests/test_database_manager.py
+++ tests/test_database_manager.py
@@ -21,9 +21,11 @@ class TestDatabaseManager(unittest.TestCase):
     def setUp(self):
         self.db.create_tables()
         self.student = Student('Luigi', 'luigi@gmail.com', '12345678910', '123456789', '123456789')
-        self.book = Book('The Lord of the Rings', 'J. R. R. Tolkien', False, False, '1954')
-        self.article = Article('The Power of Habit', '2012', False, False, 'Lalalalala', '371', 'Luigi', 'English', 'Habit, Self-help, Psychology')
-        self.magazine = Magazine('Superinteressante', '2019', False, False, 'Abril', '100', 'Portuguese', 'Science')
+        self.student2 = Student('Mario', 'mario@gmail.com', '12345678911', '123456789', '123456788')
+        self.book = Book('The Lord of the Rings', 'J. R. R. Tolkien', False, False, 1954)
+        self.book2 = Book('The Hobbit', 'J. R. R. Tolkien', False, False, '1937')
+        self.article = Article('The Power of Habit', 2012, False, False, 'Lalalalala', '371', 'Luigi', 'English', 'Habit, Self-help, Psychology')
+        self.magazine = Magazine('Superinteressante', 2019, False, False, 'Abril', '100', 'Portuguese', 'Science')
         self.librarian = Librarian('Luigi', 'luigi@gmail.com')
         self.password = '123456'
 
@@ -38,13 +40,13 @@ class TestDatabaseManager(unittest.TestCase):
 
     def test_create_and_retrieve_librarian(self):
         self.db.create_librarian(self.librarian.name, self.librarian.email, self.password)
-        librarian_db = self.db.get_librarian(self.librarian.email)
+        librarian_db = self.db.get_librarian_by_email(self.librarian.email)
         self.assertEqual(self.librarian.name, librarian_db['name'])
         self.assertEqual(self.librarian.email, librarian_db['email'])
 
     def test_create_and_retrieve_student(self):
         self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
-        student_db = self.db.get_student(self.student.registration)
+        student_db = self.db.get_student_by_registration(self.student.registration)
         self.assertEqual(self.student.name, student_db['name'])
         self.assertEqual(self.student.email, student_db['email'])
         self.assertEqual(self.student.cpf, student_db['cpf'])
@@ -59,7 +61,7 @@ class TestDatabaseManager(unittest.TestCase):
 
     def test_create_and_retrieve_book(self):
         self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.assertEqual(self.book.title, book_db['title'])
         self.assertEqual(self.book.author, book_db['author'])
         self.assertEqual(self.book.release_year, book_db['release_year'])
@@ -69,50 +71,50 @@ class TestDatabaseManager(unittest.TestCase):
     def test_lend_book_to_student(self):
         self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
         self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.db.lend_item(book_db['id'], self.student.registration)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.assertTrue(book_db['is_lend'])
 
     def test_return_item_from_student(self):
         self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
         self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.db.lend_item(book_db['id'], self.student.registration)
         self.db.return_item(book_db['id'])
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.assertFalse(book_db['is_lend'])
 
     def test_reserve_book_to_student(self):
         self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
         self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.db.reserve_item(book_db['id'], self.student.registration)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.assertTrue(book_db['is_reserved'])
 
     def test_cancel_book_reservation_from_student(self):
         self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
         self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.db.reserve_item(book_db['id'], self.student.registration)
         self.db.cancel_item_reservation(book_db['id'])
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.assertFalse(book_db['is_reserved'])
 
     def test_get_student_who_borrowed_book(self):
         self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
         self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.db.lend_item(book_db['id'], self.student.registration)
         student_db = self.db.get_student_who_borrowed_item(book_db['id'])
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.assertEqual(self.student.name, student_db['name'])
 
     def test_get_student_who_reserved_book(self):
         self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
         self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
-        book_db = self.db.get_book_item(1)
+        book_db = self.db.get_book_item_by_id(1)
         self.db.reserve_item(book_db['id'], self.student.registration)
         student_db = self.db.get_student_who_reserved_item(book_db['id'])
         self.assertEqual(self.student.name, student_db['name'])
@@ -126,7 +128,7 @@ class TestDatabaseManager(unittest.TestCase):
                                     author=self.article.author,
                                     language=self.article.language,
                                     keywords=self.article.keywords)
-        article_db = self.db.get_article_item(1)
+        article_db = self.db.get_article_item_by_id(1)
         self.assertEqual(self.article.title, article_db['title'])
         self.assertEqual(self.article.release_year, article_db['release_year'])
         self.assertEqual(int(self.article.word_count), article_db['word_count'])
@@ -147,9 +149,9 @@ class TestDatabaseManager(unittest.TestCase):
                                     author=self.article.author,
                                     language=self.article.language,
                                     keywords=self.article.keywords)
-        article_db = self.db.get_article_item(1)
+        article_db = self.db.get_article_item_by_id(1)
         self.db.lend_item(article_db['id'], self.student.registration)
-        article_db = self.db.get_article_item(1)
+        article_db = self.db.get_article_item_by_id(1)
         self.assertTrue(article_db['is_lend'])
 
     def test_return_article_from_student(self):
@@ -162,10 +164,10 @@ class TestDatabaseManager(unittest.TestCase):
                                     author=self.article.author,
                                     language=self.article.language,
                                     keywords=self.article.keywords)
-        article_db = self.db.get_article_item(1)
+        article_db = self.db.get_article_item_by_id(1)
         self.db.lend_item(article_db['id'], self.student.registration)
         self.db.return_item(article_db['id'])
-        article_db = self.db.get_article_item(1)
+        article_db = self.db.get_article_item_by_id(1)
         self.assertFalse(article_db['is_lend'])
 
     def test_create_and_get_magazine(self):
@@ -175,7 +177,7 @@ class TestDatabaseManager(unittest.TestCase):
                                     pages_count=self.magazine.pages_count,
                                     genre=self.magazine.genre,
                                     language=self.magazine.language)  
-        magazine_db = self.db.get_magazine_item(1)
+        magazine_db = self.db.get_magazine_item_by_id(1)
         self.assertEqual(self.magazine.title, magazine_db['title'])
         self.assertEqual(self.magazine.release_year, magazine_db['release_year'])
         self.assertEqual(int(self.magazine.pages_count), magazine_db['pages_count'])
@@ -184,6 +186,76 @@ class TestDatabaseManager(unittest.TestCase):
         self.assertEqual(self.magazine.language, magazine_db['language'])
         self.assertEqual(self.magazine.is_lend, magazine_db['is_lend'])
         self.assertEqual(self.magazine.is_reserved, magazine_db['is_reserved'])
+
+    def test_get_all_students_who_borrowed_item(self):
+        self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
+        self.db.create_student(self.student2.name, self.student2.email, self.student2.cpf, self.student2.tel, self.student2.registration)
+        self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
+        self.db.create_book_item(self.book2.title, self.book2.author, self.book2.release_year)
+        book_db = self.db.get_book_item_by_id(1)
+        book_db2 = self.db.get_book_item_by_id(2)
+        self.db.lend_item(book_db['id'], self.student.registration)
+        self.db.lend_item(book_db2['id'], self.student2.registration)
+        students_db = self.db.get_all_students_who_borrowed_item()
+        self.assertEqual(2, len(students_db))
+
+    def test_get_all_students_who_reserved_item(self):
+        self.db.create_student(self.student.name, self.student.email, self.student.cpf, self.student.tel, self.student.registration)
+        self.db.create_student(self.student2.name, self.student2.email, self.student2.cpf, self.student2.tel, self.student2.registration)
+        self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
+        self.db.create_book_item(self.book2.title, self.book2.author, self.book2.release_year)
+        book_db = self.db.get_book_item_by_id(1)
+        book_db2 = self.db.get_book_item_by_id(2)
+        self.db.reserve_item(book_db['id'], self.student.registration)
+        self.db.reserve_item(book_db2['id'], self.student2.registration)
+        students_db = self.db.get_all_students_who_reserved_item()
+        self.assertEqual(2, len(students_db))
+
+    def test_update_book_item(self):
+        self.db.create_book_item(self.book.title, self.book.author, self.book.release_year)
+        book_db = self.db.get_book_item_by_id(1)
+        self.db.update_book_item(book_db['id'], title='Novo titulo', author='Novo autor', release_year=2018)
+        book_db = self.db.get_book_item_by_id(1)
+        self.assertEqual('Novo titulo', book_db['title'])
+        self.assertEqual('Novo autor', book_db['author'])
+        self.assertEqual(2018, book_db['release_year'])
+
+    def test_update_article_item(self):
+        self.db.create_article_item(
+                                    release_year=self.article.release_year,
+                                    title=self.article.title,
+                                    abstract=self.article.abstract,
+                                    word_count=self.article.word_count,
+                                    author=self.article.author,
+                                    language=self.article.language,
+                                    keywords=self.article.keywords)
+        article_db = self.db.get_article_item_by_id(1)
+        self.db.update_article_item(article_db['id'], release_year=2018, title='Novo titulo', abstract='Novo abstract', word_count=100, author='Novo autor', language='Novo idioma', keywords='Novas palavras')
+        article_db = self.db.get_article_item_by_id(1)
+        self.assertEqual(2018, article_db['release_year'])
+        self.assertEqual('Novo titulo', article_db['title'])
+        self.assertEqual('Novo abstract', article_db['abstract'])
+        self.assertEqual(100, article_db['word_count'])
+        self.assertEqual('Novo autor', article_db['author'])
+        self.assertEqual('Novo idioma', article_db['language'])
+        self.assertEqual('Novas palavras', article_db['keywords'])
+
+    def test_update_magazine_item(self):
+        self.db.create_magazine_item(release_year=self.magazine.release_year,
+                                    title=self.magazine.title,
+                                    publisher=self.magazine.publisher,
+                                    pages_count=self.magazine.pages_count,
+                                    genre=self.magazine.genre,
+                                    language=self.magazine.language)
+        magazine_db = self.db.get_magazine_item_by_id(1)
+        self.db.update_magazine_item(magazine_db['id'], release_year=2018, title='Novo titulo', publisher='Nova editora', pages_count=100, genre='Novo genero', language='Novo idioma')
+        magazine_db = self.db.get_magazine_item_by_id(1)
+        self.assertEqual(2018, magazine_db['release_year'])
+        self.assertEqual('Novo titulo', magazine_db['title'])
+        self.assertEqual('Nova editora', magazine_db['publisher'])
+        self.assertEqual(100, magazine_db['pages_count'])
+        self.assertEqual('Novo genero', magazine_db['genre'])
+        self.assertEqual('Novo idioma', magazine_db['language'])
 
 if __name__ == '__main__':
     unittest.main()

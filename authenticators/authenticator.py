8e938fa93a18bcbb8726b3a8c588d27aa2d687e4
Luigi Remor <luigiremor@gmail.com>
2023-06-25 17:06:11 -0300

    refactor: adding staticmethod

--- authenticators/authenticator.py
+++ authenticators/authenticator.py
@@ -5,6 +5,7 @@ import os
 
 class Authenticator:
 
+    @staticmethod
     def hash_password(password):
         """Hash a password for storing."""
         salt = hashlib.sha256(os.urandom(60)).hexdigest().encode('ascii')
@@ -13,6 +14,7 @@ class Authenticator:
         pwdhash = binascii.hexlify(pwdhash)
         return (salt + pwdhash).decode('ascii')
 
+    @staticmethod
     def verify_password(stored_password, provided_password):
         """Verify a stored password against one provided by user"""
         salt = stored_password[:64]

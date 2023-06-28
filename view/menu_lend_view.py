78ceb83855c3de9a8c89189eb36b59c83a44fa8b
Luigi Remor <luigiremor@gmail.com>
2023-06-28 12:34:28 -0300

    feat: wip forms lendings combobox dynamic

--- view/menu_lend_view.py
+++ view/menu_lend_view.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import customtkinter
 from components.ctk_listbox import CTkListbox
 from controller.library_controller import LibraryController
@@ -43,6 +44,36 @@ class MenuLend(customtkinter.CTkFrame):
         self.details_frame = customtkinter.CTkFrame(self)
         self.details_frame.pack(side=tk.RIGHT, fill=tk.Y)
 
-        self.lends_listbox = CTkListbox(
-            self, width=200, height=300, command=None)
-        self.lends_listbox.pack(fill=tk.BOTH, expand=True)
+        students = [
+            f"{student['registration']} - {student['name']}" for student in self.controller.get_all_students()]
+
+        self.students_combobox = customtkinter.CTkComboBox(
+            self.details_frame, values=students, command=None)
+        self.students_combobox.pack(side=tk.LEFT, fill=tk.X, padx=5, pady=5)
+
+        self.item_type_combobox = customtkinter.CTkComboBox(
+            self.details_frame, values=['Book', 'Magazine', 'Article'], command=self.update_items_combobox)
+        self.item_type_combobox.set('Book')
+        self.item_type_combobox.pack(side=tk.LEFT, fill=tk.X, padx=5, pady=5)
+
+        self.items_combobox = customtkinter.CTkComboBox(
+            self.details_frame, values=[], command=None)
+        self.items_combobox.pack(side=tk.LEFT, fill=tk.X, padx=5, pady=5)
+
+    def update_items_combobox(self, event=None):
+        selected_type = self.item_type_combobox.get()
+        items = self.controller.get_all_items_by_type(selected_type)
+
+        # Check if items is not None before the list comprehension
+        if items:
+            items_values = [
+                f"{item['id_item']} - {item['title']}" for item in items]
+
+            self.items_combobox.destroy()
+            self.items_combobox = customtkinter.CTkComboBox(
+                self.details_frame, values=items_values, command=None)
+        else:
+            self.items_combobox.destroy()
+            self.items_combobox = customtkinter.CTkComboBox(
+                self.details_frame, values=[f'No {selected_type} available'], command=None)
+        self.items_combobox.pack(side=tk.LEFT, fill=tk.X, padx=5, pady=5)

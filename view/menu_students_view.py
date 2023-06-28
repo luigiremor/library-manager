60cf8d81899fa7096cc19c038b5e5e4618a00733
Erik Orsolin <depaulaerik670@gmail.com>
2023-06-28 16:32:15 -0300

    feat: decoupling UpdateStudentForm class from MenuStudent

--- view/menu_students_view.py
+++ view/menu_students_view.py
@@ -72,6 +72,7 @@ class MenuStudents(ctk.CTkFrame):
             registration_selected)
 
         if student:
+            from view.update_student_form import UpdateStudentForm
             UpdateStudentForm(self, student=student)
 
     def delete_student(self):
@@ -95,81 +96,3 @@ class MenuStudents(ctk.CTkFrame):
                                      text=f"{key}: {value}")
                 label.pack()
 
-
-
-class UpdateStudentForm(ctk.CTkToplevel):
-    def __init__(self, parent: MenuStudents, student: dict):
-        super().__init__(parent)
-        self.parent = parent
-        self.controller: LibraryController = parent.controller
-        self.title = 'Update Student'
-        self.student = student
-
-        self.create_widgets(student=student)
-
-    def create_widgets(self, student):
-        self.name_label = ctk.CTkLabel(self, text="Name")
-        self.name_label.grid(row=1, column=0, padx=10, pady=5)
-
-        self.name_entry = ctk.CTkEntry(self)
-        self.name_entry.grid(row=1, column=1, padx=10, pady=5)
-        self.name_entry.insert(0, student['name'])
-
-        self.email_label = ctk.CTkLabel(self, text="Email")
-        self.email_label.grid(row=2, column=0, padx=10, pady=5)
-
-        self.email_entry = ctk.CTkEntry(self)
-        self.email_entry.grid(row=2, column=1, padx=10, pady=5)
-        self.email_entry.insert(0, student['email'])
-
-        self.cpf_label = ctk.CTkLabel(self, text="CPF")
-        self.cpf_label.grid(row=3, column=0, padx=10, pady=5)
-
-        self.cpf_entry = ctk.CTkEntry(self)
-        self.cpf_entry.grid(row=3, column=1, padx=10, pady=5)
-        self.cpf_entry.insert(0, student['cpf'])
-
-        self.tel_label = ctk.CTkLabel(self, text="Telephone")
-        self.tel_label.grid(row=4, column=0, padx=10, pady=5)
-
-        self.tel_entry = ctk.CTkEntry(self)
-        self.tel_entry.grid(row=4, column=1, padx=10, pady=5)
-        self.tel_entry.insert(0, student['tel'])
-
-        self.registration_label = ctk.CTkLabel(self, text="Registration")
-        self.registration_label.grid(row=5, column=0, padx=10, pady=5)
-
-        self.registration_entry = ctk.CTkEntry(self)
-        self.registration_entry.grid(row=5, column=1, padx=10, pady=5)
-        self.registration_entry.insert(0, student['registration'])
-
-        self.button_frame = ctk.CTkFrame(self)
-        self.button_frame.grid(row=6, column=0, columnspan=2, padx=10, pady=5)
-
-        self.submit_button = ctk.CTkButton(
-            self.button_frame, text="Update", command=self.update_student)
-        self.submit_button.pack(side=tk.LEFT, padx=5)
-
-        self.cancel_button = ctk.CTkButton(
-            self.button_frame, text="Cancel", command=self.destroy)
-        self.cancel_button.pack(side=tk.LEFT, padx=5)
-
-    def update_student(self):
-        name = self.name_entry.get()
-        email = self.email_entry.get()
-        cpf = self.cpf_entry.get()
-        tel = self.tel_entry.get()
-        registration = self.registration_entry.get()
-        if name and email and cpf and tel and registration:
-            self.controller.update_student(
-                student_id=self.student['id'],
-                name=name,
-                email=email,
-                cpf=cpf,
-                tel=tel,
-                registration=registration
-            )
-            self.parent.refresh_students()
-            self.destroy()
-        else:
-            messagebox.showerror('Error', 'Fill all fields')

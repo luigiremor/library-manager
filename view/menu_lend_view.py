adc56be30d36391dbc3996523300f8d9ecfca7b9
Erik Orsolin <depaulaerik670@gmail.com>
2023-06-27 22:26:26 -0300

    feat: create widgets on menu lend

--- view/menu_lend_view.py
+++ view/menu_lend_view.py
@@ -1,7 +1,49 @@
-import tkinter
+import tkinter as tk
 import customtkinter
+from components.ctk_listbox import CTkListbox
+from controller.library_controller import LibraryController
 
 class MenuLend(customtkinter.CTkFrame):
-    def __init__(self, parent):
-        super().__init__(parent, height=650, width=1000)
+    def __init__(self, parent, controller: LibraryController):
+        super().__init__(parent)
         self.parent = parent
+        self.controller = controller
+        self.grid(sticky="nsew")
+        
+        # configure grid to fill window
+        tk.Grid.columnconfigure(self, 0, weight=1)
+        tk.Grid.rowconfigure(self, 0, weight=1)
+
+        self.create_widgets()
+    
+
+    def create_widgets(self):
+        self.title_label = customtkinter.CTkLabel(self, text='Lend List')
+        self.title_label.pack(fill=tk.X, padx=5, pady=5)
+
+        self.buttons_frame = customtkinter.CTkFrame(self)
+        self.buttons_frame.pack(fill=tk.X, padx=5, pady=5)
+
+        self.refresh_button = customtkinter.CTkButton(
+            self.buttons_frame, text='Refresh', command=None)
+        self.refresh_button.pack(side=tk.LEFT, padx=5)
+
+        self.new_button = customtkinter.CTkButton(
+            self.buttons_frame, text='New Lend', command=None)
+        self.new_button.pack(side=tk.LEFT, padx=5)
+
+        self.update_button = customtkinter.CTkButton(
+            self.buttons_frame, text='Update Lend', command=None)
+        self.update_button.pack(side=tk.LEFT, padx=5)
+
+        self.delete_button = customtkinter.CTkButton(
+            self.buttons_frame, text='Delete Lend', command=None)
+        self.delete_button.pack(side=tk.LEFT, padx=5)
+
+        self.details_frame = customtkinter.CTkFrame(self)
+        self.details_frame.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.lends_listbox = CTkListbox(
+            self, width=200, height=300, command=None)
+        self.lends_listbox.pack(fill=tk.BOTH, expand=True)
+
